7528ea1c353c8203eb5e0a3e70d7336f04920eb2
Daniel Habib <daniel.habib@dataprev.gov.br>
2021-09-11 03:06:23 -0300

    WIP trying to make quil work

--- src/karel/core.clj
+++ src/karel/core.clj
@@ -52,10 +52,12 @@
    :chips [(entity 1 1) (entity 3 3)]
    :walls
      (concat
-       (make-horizontal-line 0 0 5)
-       (make-horizontal-line 5 0 5)
-       (make-vertical-line 0 0 5)
-       (make-vertical-line 5 0 5))})
+       (make-horizontal-line 0 0 20)
+       (make-horizontal-line 20 0 20)
+       (make-vertical-line 0 0 20)
+       (make-vertical-line 20 0 20))})
+(map pos
+     (:walls s1))
 
 (comment
   {:karel {:x 0 :y 0 :angle 0}
@@ -78,96 +80,80 @@
   (turn
     (step [(entity 1 0) (entity 1 1)])))
 
-; ;; A4 paper size
-; (defn mm-to-px
-;   [mm]
-;   (/ (* mm 72) 25.4))
+(def W 800)
+(def H 600)
+(def ENTER 10)
 
-; (def W (int (mm-to-px 210)))
-; (def H (int (mm-to-px 297)))
-; (def ENTER 10)
+(defn setup "returns the initial state" []
+  ; Set frame rate frames per second.
+  (q/frame-rate 10)
+  ; Set color mode to HSB (HSV) instead of default RGB.
+  (q/color-mode :hsb)
+  ; setup function returns initial state. It contains
+  ; circle color and position.
+  {:image (q/load-image "resources/test.png")
+   :s s1})
 
-; (defn calculate-pos [w h qtd-w qtd-h size-w size-h]
-;   (let [
-;         h-size (max size-h (quot h qtd-h))
-;         w-size (max size-w (quot w qtd-w))]
-;     (let [ys (filter #(= 0 (rem % h-size)) (range h))
-;           xs (filter #(= 0 (rem % w-size)) (range w))]
-;       (for [x xs y ys]
-;         [x y]))))
+(defn key-to-offset-w [key]
+  (case (:key key)
+    :left dec
+    :right inc
+    identity))
 
-; (defn setup [] "returns the init state"
-;   ; Set frame rate frames per second.
-;   (q/frame-rate 10)
-;   ; Set color mode to HSB (HSV) instead of default RGB.
-;   (q/color-mode :hsb)
-;   ; setup function returns initial state. It contains
-;   ; circle color and position.
-;   {:image (q/load-image "resources/test.png")
-;    :state s1})
+(defn key-to-offset-h [key]
+  (case (:key key)
+    :up dec
+    :down inc
+    identity))
 
-; (defn key-to-offset-w [key]
-;   (case (:key key)
-;     :left dec
-;     :right inc
-;     identity))
+(defn the-key-handler [state k]
+  (assoc state
+    :s (:s state)
+    :done (= ENTER (:key-code k))))
 
-; (defn key-to-offset-h [key]
-;   (case (:key key)
-;     :up dec
-;     :down inc
-;     identity))
+(defn f-all-pos [walls]
+  (map (fn [w] (pos w)) walls))
 
-; (defn the-key-handler [state k]
-;   (assoc state
-;     :qtd-w (max 1 ((key-to-offset-w k) (:qtd-w state)))
-;     :qtd-h (max 1 ((key-to-offset-h k) (:qtd-h state)))
-;     :done (= ENTER (:key-code k))))
+(defn update [state]
+  (let [img (:image state)]
+    (if (q/loaded? img)
+      {:image img
+       :s (:s state)
+       :done (:done state)}
+      {:image img
+       :s (:s state)
+       :done (:done state)})))
 
-; (defn update [state]
-;   (let [img (:image state)
-;         next {:image img}
-;         qtd-w (:qtd-w state)
-;         qtd-h (:qtd-h state)]
-;     (if (q/loaded? img)
-;       {:image img
-;        :all-pos (calculate-pos W H qtd-w qtd-h (.width img) (.height img))
-;        :qtd-w qtd-w
-;        :qtd-h qtd-h
-;        :done (:done state)}
-;       {:image img
-;        :qtd-w qtd-w
-;        :qtd-h qtd-h
-;        :done (:done state)})))
+(defn draw-labels [state]
+  (let [img (:image state)]
+    (when-let [all-pos (f-all-pos (:walls (:s state)))]
+      (do
+        (println "DAAAAAAAAAAAAAAAAA" all-pos)
+        (flush)
+        (q/background 255)
+        (doall
+          (map #(q/image img (first %) (second %)) all-pos))))))
 
-; (defn draw-labels [state]
-;   (let [img (:image state)]
-;     (when-let [all-pos (:all-pos state)]
-;       (do
-;         (q/background 255)
-;         (doall
-;           (map #(q/image img (first %) (second %)) all-pos))))))
+(defn draw [state]
+  (when (:all-pos state)
+    (if (:done state)
+      (do
+        (q/do-record (q/create-graphics W H :pdf "out.pdf")
+                     (draw-labels state))
+        (q/exit))
+      (draw-labels state))))
 
-; (defn draw [state]
-;   (when (:all-pos state)
-;     (if (:done state)
-;       (do
-;         (q/do-record (q/create-graphics W H :pdf "out.pdf")
-;                      (draw-labels state))
-;         (q/exit))
-;       (draw-labels state))))
-
-; (q/defsketch label-maker
-;   :title "karel"
-;   :size [W H]
-;   ; setup function called only once, during sketch initialization.
-;   :setup setup
-;   ; update is called on each iteration before draw.
-;   :update update
-;   :draw draw
-;   :features [:keep-on-top]
-;   :key-pressed the-key-handler
-;   ; This sketch uses functional-mode middleware.
-;   ; Check quil wiki for more info about middlewares and particularly
-;   ; fun-mode.
-;   :middleware [m/fun-mode m/pause-on-error])
+(q/defsketch label-maker
+  :title "karel"
+  :size [W H]
+  ; setup function called only once, during sketch nitialization.
+  :setup setup
+  ; update is called on each iteration before draw.
+  :update update
+  :draw draw
+  :features [:keep-on-top]
+  :key-pressed the-key-handler
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
+  :middleware [m/fun-mode m/pause-on-error])
