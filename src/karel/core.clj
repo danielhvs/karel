57ec61c06f0c0859148675852cb9450c524ddf81
Daniel Habib <daniel.habib@dataprev.gov.br>
2021-09-11 01:21:53 -0300

    add turn and update walk

--- src/karel/core.clj
+++ src/karel/core.clj
@@ -2,8 +2,14 @@
   (:require
     [clojure.string :as str]))
 
+(def walk-dir
+  {0 {:x inc}
+   90 {:y inc}
+   180 {:x dec}
+   270 {:y dec}})
+
 (defn entity [x y t]
-  {:x x :y y :type t})
+  {:x x :y y :type t :angle 0})
 
 (defn karel [x y]
   (entity x y :karel))
@@ -11,8 +17,15 @@
 (defn wall [x y]
   (entity x y :wall))
 
-(defn walk [entity]
-  (update entity :x inc))
+(defn walk [{:keys [angle] :as entity}]
+  (let [dir-fn (get walk-dir angle)
+        dir (first (keys dir-fn))
+        walk-fn (first (vals dir-fn))]
+    (update entity dir walk-fn)))
+
+(defn turn [{:keys [angle] :as entity}]
+  (let [next-angle (+ angle 90)]
+    (assoc entity :angle (mod next-angle 360))))
 
 (defn step "state is a vector of entities"
   [state]
@@ -25,4 +38,8 @@
 
 (comment
   (step [(wall 0 0)])
-  (step [(karel 1 0) (wall 1 1)]))
+  (for [x (range 0 700 30)]
+    (mod x 360))
+  (turn (turn (karel 1 0)))
+  (turn
+    (step [(karel 1 0) (wall 1 1)])))
