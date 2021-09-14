ad11ab7ccea4a7396616952968eeb62e082132c9
Daniel Habib <daniel.habib@dataprev.gov.br>
2021-09-14 00:29:35 -0300

    drawing based on z axis

--- src/karel/core.clj
+++ src/karel/core.clj
@@ -9,10 +9,13 @@
    180 {:x dec}
    270 {:y dec}})
 
-(defn entity [x y]
-  {:x x :y y :z 0 :angle 0})
+(defn entity
+  ([x y z a]
+   {:x x :y y :z z :angle a})
+  ([x y]
+   (entity x y 0 0)))
 
-(defn pos [entity]
+(defn pos-xy [entity]
   [(:x entity) (:y entity)])
 
 (defn get-karel [state]
@@ -20,8 +23,8 @@
 
 (defn on-chip? [state]
   (let [karel (get-karel state)
-        k-pos (pos karel)]
-    (first (filter #(= k-pos (pos %)) (:chips state)))))
+        k-pos (pos-xy karel)]
+    (first (filter #(= k-pos (pos-xy %)) (:chips state)))))
 
 (defn turn [state]
   (let [karel (get-karel state)
@@ -40,7 +43,7 @@
         walk-fn (first (vals dir-fn))
         next-karel (update karel dir walk-fn)]
     (if (seq (filter
-               (fn [w] (= (pos next-karel) (pos w)))
+               (fn [w] (= (pos-xy next-karel) (pos-xy w)))
                (:walls state)))
       state
       (assoc state :karel [next-karel]))))
@@ -56,19 +59,16 @@
 (defn grab [state]
   (if-let [chip (on-chip? state)]
     (let [chips (:chips state)
-          new-chip (update chip :z inc)]
+          new-chip (assoc chip :z 1)]
       (assoc state :chips
           (conj
             (remove (fn [c] (= chip c)) chips)
             new-chip)))
     state))
 
-(defn ->pos [entities]
-  (map (fn [e] (pos e)) entities))
-
 (def s1
   {:karel [(entity 1 1)]
-   :chips [(entity 2 1)]
+   :chips [(entity 2 1 -1 0)]
    :goals [(entity 6 2)]
    :walls
      (concat
@@ -78,8 +78,9 @@
        (make-vertical-line 7 0 3))})
 
 (comment
-  (grab {:karel [(entity 1 1)]
-         :chips [(entity 1 1) (entity 2 1)]})
+  (grab
+    (grab {:karel [(entity 1 1)]
+           :chips [(entity 1 1) (entity 2 1)]}))
   (let [state {:karel [(entity 1 0)]
                :chips [(entity 2 0) (entity 3 0) (entity 1 0)]
                :walls [(entity 2 0) (entity 3 0)]}]
@@ -93,21 +94,19 @@
   (turn
     (step s1)))
 
-(def L 40)
+(def L 32)
 (def W 800)
 (def H 600)
 
-(defn point->quil [[x y] length]
-  [(* x length)
-   (* y length)])
+(defn point->quil [entity length]
+  (assoc entity
+    :x (* (:x entity) length)
+    :y (* (:y entity) length)))
 
-(defn points->quil [points length]
-  (map (fn [p] (point->quil p length)) points))
+(defn points->quil [entities length]
+  (map (fn [e] (point->quil e length)) entities))
 
 (defn setup "returns the initial state" []
-  (q/stroke 0xffa8d0db)
-  (q/stroke 255 255 0)
-  (q/stroke-weight 1)
   {:scenario s1
    :karel (q/load-image "resources/head.png")
    :walls (q/load-image "resources/box32.png")
@@ -117,29 +116,39 @@
 (defn the-key-handler [{:keys [scenario] :as state} k]
   (assoc state :scenario
       (case (:key-code k)
-        71 (grab scenario) ; G 
-        74 (turn scenario) ; J 
-        75 (step scenario) ; K 
+        71 (grab scenario) ; G
+        74 (turn scenario) ; J
+        75 (step scenario) ; K
         scenario)))
 
 (defn update-state [state]
   state)
 
 (defn draw-element "{:type [x y]}"
-  [[x y] img]
+  [{:keys [x y]} img]
   (q/image img x y))
+
+(defn sort-to-draw [scenario]
+  (let [res
+          (for [kind (keys scenario)]
+            (map (fn [e] {:type kind
+                          :x (:x e)
+                          :y (:y e)
+                          :z (:z e)
+                          :angle (:angle e)}) (kind scenario)))]
+    (->> res
+         (reduce #(into %1 %2) [])
+         (sort-by :z))))
 
 (defn draw-state [state]
   (q/clear)
   (q/background 255 255 255)
-  (doseq [kind [:walls :chips :karel :goals]]
-    (let [scenario (-> state :scenario)
-          points (->pos (-> scenario
-                            kind))]
-      (doseq [[x y] (points->quil points L)]
-        (draw-element [x y] (kind state))))))
+  (let [scenario (-> state :scenario)
+        entities (sort-to-draw scenario)]
+    (doseq [e (points->quil entities L)]
+      (draw-element e ((:type e) state)))))
 
-(q/defsketch label-maker
+(q/defsketch karel-sketch
   :title "karel"
   :size [W H]
   ; setup function called only once, during sketch nitialization.
